388e77f3d1d7977c501ff5187a315ccdc85e0cb1
AKChervyakov <chervyakov1912@gmail.com>
2017-02-04 12:27:17 +0300

    Исправлен тест для класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,11 +8,17 @@ import org.testng.annotations.Test;
  */
 public class PointTests {
 
-  @Test
+  @Test(priority = 0)
   public void testDistance() {
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(1,2);
-    Assert.assertEquals(p1.distance(p2), 1.0); //положительный тест
-    Assert.assertEquals(p1.distance(p2), 2.0); //отрицательный тест
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(1, 2);
+    Assert.assertEquals(p1.distance(p2), 1.0); //проверяем, что расстояние м-у точками равено 1.0
+  }
+
+  @Test(priority = 1)
+  public void testDistance2() {
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(1, 2);
+    Assert.assertNotEquals(p1.distance(p2), 2.0); //Проверяем, что расстояние м-у точками не равено 2.0
   }
 }
