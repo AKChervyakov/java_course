3ed3866e262db07fb27e082b35d27f7a2ed06f47
AKChervyakov <chervyakov1912@gmail.com>
2017-03-25 15:33:25 +0300

    Реализовано обращение к контактам при помощи ORM (l7_m3)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,67 +1,144 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import com.sun.javafx.beans.IDProperty;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
+@Entity
+@Table (name = "addressbook")
 @XStreamAlias("contact")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;;
+
   @Expose
+  @Column(name = "firstname")
   private String first_name;
+
   @Expose
+  @Column(name = "middlename")
   private String middle_name;
+
   @Expose
+  @Column(name = "lastname")
   private String last_name;
+
   @Expose
+  @Column(name = "nickname")
   private String nickname;
+
   @Expose
+  @Column(name = "title")
   private String title;
+
   @Expose
+  @Column(name = "company")
   private String company;
+
   @Expose
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String telephone_Home;
+
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String telephone_Mobile;
+
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String telephone_Work;
+
   @Expose
+  @Column(name = "fax")
+  @Type(type = "text")
   private String fax;
+
   @Expose
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
+
   @Expose
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email2;
+
   @Expose
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email3;
+
   @Expose
+  @Column(name = "homepage")
+  @Type(type = "text")
   private String homepage;
+
+  @Transient
   private String birthday_day;
+
+  @Transient
   private String birthday_day_ent;
+
+  @Transient
   private String birthday_mon;
+
+  @Transient
   private String birthday_mon_ent;
+
+  @Transient
   private String birthday_year;
+
+  @Transient
   private String anniversary_day;
+
+  @Transient
   private String anniversary_day_ent;
+
+  @Transient
   private String anniversary_mon;
+
+  @Transient
   private String anniversary_mon_ent;
+
+  @Transient
   private String anniversary_year;
+
+  @Transient
   private String group;
+
+  @Transient
   private String allPhones;
+
+  @Transient
   private String allEmails;
+
+  @Transient
   private String allInformation;
-  private File photo;
+
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
