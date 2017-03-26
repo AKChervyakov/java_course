5a06a8dc9864c3c678d10aa06670edfe00707428
AKChervyakov <chervyakov1912@gmail.com>
2017-03-26 20:04:41 +0300

    Исправлен тест модификации контакта для ДЗ 17.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -24,14 +24,7 @@ public class ContactModificationTests extends TestBase {
                       .withTitle("title1").withCompany("Company1").withAddress("c. Saratov, st. Gagarina 32, fl. 59")
                       .withTelephone_Home("493-44-33").withTelephone_Mobile("+7(834)390-78-49").withTelephone_Work("4931919").withFax("-")
                       .withEmail("petrov@mail.ru").withEmail2("PPPetrov@gmail.com").withEmail3("Petrov38@yande.ru")
-                      .withHomepage("-").withBirthday_day("//div[@id='content']/form/select[1]//option[19]")
-                      .withBirthday_day_ent("//div[@id='content']/form/select[1]//option[19]")
-                      .withBirthday_mon("//div[@id='content']/form/select[2]//option[9]")
-                      .withBirthday_mon_ent("//div[@id='content']/form/select[2]//option[9]").withBirthday_year("1988")
-                      .withAnniversary_day("//div[@id='content']/form/select[3]//option[12]")
-                      .withAnniversary_day_ent("//div[@id='content']/form/select[3]//option[12]")
-                      .withAnniversary_mon("//div[@id='content']/form/select[4]//option[12]")
-                      .withAnniversary_mon_ent("//div[@id='content']/form/select[4]//option[12]").withAnniversary_year("2013").withGroup("test1")
+                      .withHomepage("-")
               , true);
     }
   }
@@ -46,14 +39,7 @@ public class ContactModificationTests extends TestBase {
             .withTitle("title1").withCompany("Company1").withAddress("c. Saratov, st. Gagarina 32, fl. 59")
             .withTelephone_Home("4955588").withTelephone_Mobile("+7834390749").withTelephone_Work("4931919").withFax("-")
             .withEmail("petrov@mail.ru").withEmail2("PPPetrov@gmail.com").withEmail3("Petrov38@yande.ru")
-            .withHomepage("-").withBirthday_day("//div[@id='content']/form/select[1]//option[19]")
-            .withBirthday_day_ent("//div[@id='content']/form/select[1]//option[19]")
-            .withBirthday_mon("//div[@id='content']/form/select[2]//option[9]")
-            .withBirthday_mon_ent("//div[@id='content']/form/select[2]//option[9]").withBirthday_year("1988")
-            .withAnniversary_day("//div[@id='content']/form/select[3]//option[12]")
-            .withAnniversary_day_ent("//div[@id='content']/form/select[3]//option[12]")
-            .withAnniversary_mon("//div[@id='content']/form/select[4]//option[12]")
-            .withAnniversary_mon_ent("//div[@id='content']/form/select[4]//option[12]").withAnniversary_year("2013").withGroup("test1");
+            .withHomepage("-");
     app.contact().modify(contact);
     assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.db().contacts();
